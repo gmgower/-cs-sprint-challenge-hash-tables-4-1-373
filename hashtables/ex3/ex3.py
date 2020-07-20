01e9e1683eb27b9bdcab06b92b07619efaddc9ab
@gmgower <gmgower@gmail.com>
2020-07-19 21:49:01 -0500

    ex3 & ex4.

--- hashtables/ex3/ex3.py
+++ hashtables/ex3/ex3.py
@@ -3,15 +3,45 @@ def intersection(arrays):
     YOUR CODE HERE
     """
     # Your code here
+    cache = {}
+    result = []
+    # get the inner array from the 2D array passed in
+    for array in arrays:
+        # get the value from the inner array
+        for item in array:
+            if item in cache:
+                # if the item is in the cache increment the value
+                cache[item] += 1
+            else:
+                # else set the value == 1
+                cache[item] = 1
+
+    for key, value in cache.items():
+        # for each value in the cache
+        # if the value is equal to the amount of inner arrays,
+        # append the result to the array
+        if value == len(arrays):
+            result.append(key)
+
+    # if we get here, the array is set,
+    # return it
 
     return result
 
+# if __name__ == "__main__":
+#     arrays = []
 
+#     arrays.append(list(range(1000000, 2000000)) + [1, 2, 3])
+#     arrays.append(list(range(2000000, 3000000)) + [1, 2, 3])
+#     arrays.append(list(range(3000000, 4000000)) + [1, 2, 3])
+
+#     print(intersection(arrays))
+
+    
 if __name__ == "__main__":
-    arrays = []
-
-    arrays.append(list(range(1000000, 2000000)) + [1, 2, 3])
-    arrays.append(list(range(2000000, 3000000)) + [1, 2, 3])
-    arrays.append(list(range(3000000, 4000000)) + [1, 2, 3])
-
-    print(intersection(arrays))
+    result = intersection([
+            [1,2,3],
+            [1,4,5],
+            [1,6,7]
+    ])
+    print(result == [1])
--- hashtables/ex4/ex4.py
+++ hashtables/ex4/ex4.py
@@ -3,7 +3,24 @@ def has_negatives(a):
     YOUR CODE HERE
     """
     # Your code here
+    cache = {}
+    result = []
+    # iterate through all items in the list
+    for (index, item) in enumerate(a):
+        # cache the items as they are accessed
+        cache[index] = item
 
+    # check item in a again
+    for item in a:
+        # if the item multiplied by -1 is in the cache
+        if (item * -1) in cache:
+            # if the item is 0,
+            # ignore the value because zero doesn't have a negative
+            if item == 0:
+                continue
+            # else append the opposite of the item in the result list
+            result.append(-item)   
+    # return the list 
     return result
 
 
